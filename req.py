9d540a89ab1c4e44c922fe3d6c870776cd2624e8
joel bispo <joelbispo04@hotmail.com>
2023-01-17 09:25:58 -0300

    primeiro commit

---
+++ req.py
@@ -0,0 +1,11 @@
+import requests
+import pprint
+
+
+link = "https://servicodados.ibge.gov.br/api/v3/agregados/1278/periodos/2006/variaveis/323?localidades=N1[all]"
+
+requisição = requests.get(link)
+requi_dic = requisição.json()
+
+
+pprint.pprint(requi_dic)
